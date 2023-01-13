56cb259f88ad5640c7feaf472e2cefe70238b397
mizumizu4377 <mizutanibuying@gmail.com>
2023-01-13 20:34:50 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -17,3 +17,8 @@ const plus1 = function (x) {
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply を作成して正しく動くかテストしてください。
 
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound を作成して正しく動くかテストしてください。
+const alertTime = function () {
+  alert(new Date().toLocaleString())
+}
+
+alertTime()
