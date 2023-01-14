030dc676e3e41849b3d31ddc1d245b3fc98e52b0
mizumizu4377 <mizutanibuying@gmail.com>
2023-01-14 11:53:44 +0900

    [カウンター]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,11 @@
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+//inputText.oninput = sayHello
+// inputDate.oninput = sayHello
